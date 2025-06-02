040b06742b18b286bef502a321f8225f9bc6d3ed
Paty Rodriguez <paty-rdz-t@outlook.com>
2025-06-01 22:50:11 -0600

    Changes

--- Calculator/src/main/java/com/calculator/Main.java
+++ Calculator/src/main/java/com/calculator/Main.java
@@ -15,6 +15,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 384, 512);
+        stage.setResizable(false);
         stage.setTitle("Calculator");
         stage.getIcons().add(new Image(getClass().getResourceAsStream("/icon.png")));
         stage.setScene(scene);
